5f159b1587e4c4a2fc6a5d60ec71daa77d3e3311
svicario <saverio.vicario@gmail.com>
2018-02-01 12:08:12 +0100

    execution Vlab file

---
+++ Vlab_run.sh
@@ -0,0 +1,13 @@
+
+
+
+
+
+python phyloH/esecutorePhyloHPandas.py -M TNC -G 1  -s SAMPLE_FILENAME -g "WWF_MHTNAM" -r 100 -o out -e 0 -k 0
+
+# collect the output
+tar cvfz Fulloutput.tgz --ignore-failed-read --hard-dereference external/ out* PhyloCommunity.js sample sub* */decorated* SAMPLE_FILENAME*  
+
+tar cvfz --ignore-failed-read --hard-dereference Shapefile.tgz */decorated*
+
+tar cvfz --ignore-failed-read --hard-dereference HTMLReport.tgz external/ out* PhyloCommunity.js *.html   *.mibybranch *.TreeLabeled
